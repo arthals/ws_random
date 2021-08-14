7455202394922994c3d36eaae5271d25e5850fc5
vinicius anastacio <vini2anastacio@gmail.com>
2021-08-14 13:01:33 -0300

    a

---
+++ WebApplication2/Controllers/BueiroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BueiroController : ControllerBase
+    {
+                private readonly ILogger<BueiroController> _logger;
+
+        public BueiroController(ILogger<BueiroController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public  void InserirBueiros(Bueiro bueiro)
+        {
+            //... o codigo que faz algo com o bueiro
+        }
+    }
+}
---
+++ WebApplication2/Models/Bueiro.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class Bueiro
+    {
+        public int Id { get; set; }
+        public string Rua { get; set; }
+        public string CodLocalizacao { get; set; }
+        public DateTime DataAtivacao { get; set; }
+    }
+}
